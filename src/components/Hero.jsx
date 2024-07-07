baa263937ce23e70361c526713571f0e90d460cd
Avi Mehta <50762319+avim3hta@users.noreply.github.com>
2024-07-08 03:08:53 +0530

    year information change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Avi Mehta</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am currently in my 2nd year and I specialize<br className='sm:block hidden' />
-            in Computer Vision, 3D Visual interfaces <br className='sm:block hidden' /> and web applications
+            I am currently in my 3rd year and I specialize<br className='sm:block hidden' />
+            in Computer Vision, Training Machine learning models <br className='sm:block hidden' /> and web applications
           </p>
         </div>
       </div>
